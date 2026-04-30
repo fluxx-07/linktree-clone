5da2ada4ad2c5f0967becaa9ce3a63b277c960a0
Piyush Kumar <piyushkumar07.dev@gmail.com>
2026-05-01 04:09:01 +0530

    Fixed FIle name issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import LinkList from './components/LinkList'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 function App() {
   return (
